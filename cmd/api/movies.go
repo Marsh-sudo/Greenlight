839be64d29173d2bcec58c49b7615ff78d45ce4d
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-07-12 17:35:04 +0300

    add Insert method to create movie handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -38,6 +38,29 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.failedValidationResponse(w,r,v.Errors)
 		return
 	}
+
+	// Call the Insert() method on our movies model, passing in a pointer to the
+	// validated movie struct. This will create a record in the database and update the 
+	// movie struct with the system-generated information.
+	err = app.models.Movie.Insert(movie)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
+		return
+	}
+
+	// When sending a HTTP response, we want to include a Location header to let the
+	// client know which URL they can find the newly-created resource at. We make an
+	// empty http.Header map and then use the Set() method to add a new Location header, 
+	// interpolating the system-generated ID for our new movie in the URL.
+	headers := make(http.Header)
+	headers.Set("Location",fmt.Sprintf("/v1/movies/%d",movie.ID))
+
+	//write a JSON response with a 201 created status code,the movie data in the
+	//response body and the Location header
+	err = app.writeJSON(w,http.StatusCreated,envelope{"movie":movie},headers)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
+	}
 	
 
 	fmt.Fprintf(w, "%+v\n", input) 
