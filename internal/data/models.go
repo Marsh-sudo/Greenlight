bbf729ff837d65c03567fc6c50737f53a2655dbb
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-07-12 17:35:42 +0300

    create model struct

---
+++ internal/data/models.go
@@ -0,0 +1,22 @@
+package data
+
+import(
+	"database/sql"
+	"errors"
+)
+
+// Define a custom ErrRecordNotFound error. We'll return this from our Get() method when // looking up a movie that doesn't exist in our database.
+var (
+	ErrRecordNotFound = errors.New("record not found") 
+)
+	
+type Models struct {
+	Movie MovieModel
+}
+
+// For ease of use, we also add a New() method which returns a Models struct containing // the initialized MovieModel.
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movie: MovieModel{DB:db},
+	}
+}
