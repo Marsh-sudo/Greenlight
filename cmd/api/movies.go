30b70d3023de0733274cb6d157d8e77794e85a6f
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-07-14 05:17:23 +0300

    add delete mvovie handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,8 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
-	"net/http" 
-	
+	"net/http"
+
 	"github.com/Marsh-sudo/greenlight/internal/data"
 	"github.com/Marsh-sudo/greenlight/internal/validator"
 
@@ -96,4 +96,94 @@ func(app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) 
 
 	fmt.Fprintf(w, "show the details of movie %d\n",id)
 
+}
+
+
+func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// EXTRACT the movie ID from the URL
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w,r)
+		return
+	}
+
+	// fetch the existing movie record from the database sending a 404 NOT found
+	movie, err := app.models.Movie.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err,data.ErrRecordNotFound):
+			app.notFoundResponse(w,r)
+		default:
+			app.serverErrorResponse(w,r,err)
+		}
+		return
+	}
+
+	// declare an input struct to hold the expected data
+	var input struct{
+		Title string `json:"title"`
+		Year int32 `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres []string `json:"genres"` 
+	}
+
+	//read the JSON request body data into the input struct
+	err = app.readJSON(w,r,&input)
+	if err != nil {
+		app.badRequestResponse(w,r,err)
+		return
+	}
+
+	//copy the values from the request body to the appropriate fields of the movie
+	movie.Title=input.Title
+	movie.Year=input.Year
+	movie.Runtime=input.Runtime
+	movie.Genres=input.Genres
+
+	//validate the updated movie record sendiing the client a 422 Unprocessable Entity
+	v := validator.New()
+	if data.ValidateMovie(v,movie); !v.Valid() {
+		app.failedValidationResponse(w,r,v.Errors)
+		return
+	}
+
+	// pass the updated movie record to our new Update method
+	err = app.models.Movie.Update(movie)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
+		return
+	}
+
+	//write the updated movie record in a JSON response
+	err = app.writeJSON(w,http.StatusOK,envelope{"movie":movie},nil)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
+	}
+}
+
+func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+	//extract the movie ID from the URL
+	id,err := app.readIDParam(r)
+	if err!= nil{
+		app.notFoundResponse(w,r)
+		return
+	}
+
+	//delete the movie from the database,sending a 404 Not found response to the client
+	err = app.models.Movie.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err,data.ErrRecordNotFound):
+			app.notFoundResponse(w,r)
+		default:
+			app.serverErrorResponse(w,r,err)
+		}
+		return
+	}
+
+	//return a 200 OK status code along with a success message
+	err = app.writeJSON(w,http.StatusOK,envelope{"message":"movie successfully deleted"},nil)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
+	}
 }
