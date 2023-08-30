864635147ebfb25098576316925c31a9df89d60c
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-08-30 03:52:04 +0300

    create a variable for error handling

--- internal/data/models.go
+++ internal/data/models.go
@@ -8,6 +8,7 @@ import(
 // Define a custom ErrRecordNotFound error. We'll return this from our Get() method when // looking up a movie that doesn't exist in our database.
 var (
 	ErrRecordNotFound = errors.New("record not found") 
+	ErrEditConflict = errors.New("edit conflict")
 )
 	
 type Models struct {
