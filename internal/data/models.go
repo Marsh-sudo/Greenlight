cdaa9e016eb0c2b7fa232be49d97a8cfc33b52c3
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-10-11 03:27:56 +0300

    create users struct

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,11 +13,15 @@ var (
 	
 type Models struct {
 	Movie MovieModel
+	Tokens TokenModel
+	Users UserModel
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing // the initialized MovieModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movie: MovieModel{DB:db},
+		Tokens: TokenModel{DB:db},
+		Users: UserModel{DB: db},
 	}
 }
