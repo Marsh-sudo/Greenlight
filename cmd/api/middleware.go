8d9a61a7b7440334bdf09767b592bdf6aa253972
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-10-11 03:26:27 +0300

    create authenticate method

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"errors"
+	"strings"
 	"fmt"
 	"net"
 	"sync"
 	"net/http"
+	"time"
 	"golang.org/x/time/rate"
+	"github.com/Marsh-sudo/greenlight/internal/data"
+	"github.com/Marsh-sudo/greenlight/internal/validator"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -30,37 +35,113 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 func (app *application) ratelimit(next http.Handler) http.Handler {
 
+	type client struct {
+		limiter *rate.Limiter
+		lastSeen time.Time
+	}
+
 	var (
 		mu sync.Mutex
-		clients = make(map[string]*rate.Limiter)
+		clients = make(map[string]*client)
 	)
+
+	go func ()  {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+
+			// Loop through all clients. If they haven't been seen within the last three // minutes, delete the corresponding entry from the map.
+			for ip,client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients,ip)
+				}
+			}
+
+			mu.Unlock()
+		}
+	}()
 	
 
 	// The function we are returning is a closure, which 'closes over' the limiter // variable.
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		//extract the client's IP address from the request
-		ip,_,err := net.SplitHostPort(r.RemoteAddr)
-		if err != nil {
-			app.serverErrorResponse(w,r,err)
-			return
-		}
+		if app.config.limiter.enabled {
+			//extract the client's IP address from the request
+			ip,_,err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				app.serverErrorResponse(w,r,err)
+				return
+			}
 
-		//lock the mutex to prevent this code from being executed concurrently
-		mu.Lock()
+			mu.Lock()
+			// Check to see if the IP address already exists in the map. If it doesn't, then // initialize a new rate limiter and add the IP address and limiter to the map.
+			if _,found := clients[ip]; !found {
+				clients[ip] = &client{
+					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
+				}
+			}
 
-		// Check to see if the IP address already exists in the map. If it doesn't, then // initialize a new rate limiter and add the IP address and limiter to the map.
-		if _, found := clients[ip]; !found {
-			clients[ip] = rate.NewLimiter(2,4)
-		}
+			clients[ip].lastSeen = time.Now()
 
-		// Call the Allow() method on the rate limiter for the current IP address. If // the request isn't allowed, unlock the mutex and send a 429 Too Many Requests // response, just like before.
-		if !clients[ip].Allow() {
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w,r)
+				return
+			}
+
 			mu.Unlock()
-			app.rateLimitExceededResponse(w,r)
+		}
+
+		next.ServeHTTP(w,r)
+	})
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary","Authorization")
+
+		// Retrieve the value of the Authorization header from the request. This will // return the empty string "" if there is no such header found.
+		authorizationHeader := r.Header.Get("Authorization")
+
+		// If there is no Authorization header found, use the contextSetUser() helper // that we just made to add the AnonymousUser to the request context. Then we // call the next handler in the chain and return without executing any of the // code below
+		if authorizationHeader == "" {
+			r = app.contextSetUser(r, data.AnonymousUser)
+			next.ServeHTTP(w,r)
 			return
 		}
 
-		mu.Unlock()
+		headerParts := strings.Split(authorizationHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			app.invalidAuthenticationTokenResponse(w,r)
+			return
+		}
+
+		//extract the actual authentication token from the header parts
+		token := headerParts[1]
+
+		//validate the token to make sure it is in a sensible format
+		v := validator.New()
+
+		if data.ValidateTokenPlaintext(v,token); !v.Valid() {
+			app.invalidAuthenticationTokenResponse(w,r)
+			return
+		}
+
+		// Retrieve the details of the user associated with the authentication token, // again calling the invalidAuthenticationTokenResponse() helper if no
+// matching record was found. IMPORTANT: Notice that we are using
+// ScopeAuthentication as the first parameter here.
+		user, err := app.models.Users.GetForToken(data.ScopeAuthentication, token)
+		if err != nil {
+			switch {
+			case errors.Is(err,data.ErrRecordNotFound):
+				app.invalidAuthenticationTokenResponse(w,r)
+			default:
+				app.serverErrorResponse(w,r,err)
+			}
+			return
+		}
+
+		// Call the contextSetUser() helper to add the user information to the request // context.
+		r = app.contextSetUser(r,user)
 
 		next.ServeHTTP(w,r)
 	})
